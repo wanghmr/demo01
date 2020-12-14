58484a223b8a3d267bd61c9b19282d8704e825c2
王辉 <wangh_0929@163.com>
2020-12-14 10:42:29 +0800

    Initial commit

---
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -0,0 +1,14 @@
+package com.example.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01Application.class, args);
+        System.out.println("i am master");
+    }
+
+}
