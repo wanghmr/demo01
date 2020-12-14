e1406c6cb1af8dee1e578b7665a933150498cc99
王辉 <wangh_0929@163.com>
2020-12-14 10:45:03 +0800

    newbanch

--- src/main/java/com/example/demo01/Demo01Application.java
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -3,6 +3,9 @@ package com.example.demo01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wh
+ */
 @SpringBootApplication
 public class Demo01Application {
 
