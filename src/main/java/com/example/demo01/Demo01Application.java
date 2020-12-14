6260ebe140f9828b116f95908e8c86676d30be49
王辉 <wangh_0929@163.com>
2020-12-14 15:32:16 +0800

    wh

--- src/main/java/com/example/demo01/Demo01Application.java
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -12,7 +12,7 @@ public class Demo01Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo01Application.class, args);
         System.out.println("i am xinfenzhidsfgb");
-        System.out.println("我是新分支--------------------============");
+        System.out.println("我是新分支---------- ----------============");
     }
 
 }
