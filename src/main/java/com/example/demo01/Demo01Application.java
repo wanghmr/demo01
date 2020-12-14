234d05c7d1685e5d021ace41563213f3b6d942b4
王辉 <wangh_0929@163.com>
2020-12-14 10:44:37 +0800

    newbanch

--- src/main/java/com/example/demo01/Demo01Application.java
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -8,7 +8,7 @@ public class Demo01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo01Application.class, args);
-        System.out.println("i am master");
+        System.out.println("i am newbanch");
     }
 
 }
